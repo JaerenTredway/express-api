c59063f15644c9309856954368e2e7ef3d921419
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-06-02 09:22:57 -0600

    Added bodyParser middleware to app.js to handle PUT and POST requests.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // Require packages and set the port
 const express = require('express');
 const port = 3002;
+const bodyParser = require('body-parser');
 const app = express();
+
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
 // look for a GET request on the root (/) of the server
 app.get('/', (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.send('Hello, Server!');
+    response.send({
+        message: 'Node.js and Express REST API'}
+    );
 });
 
 // Start the server
