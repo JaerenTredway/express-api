eb4a5b9c9adf024686b88d88f65b36a6ff85b0f3
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-06-02 09:06:58 -0600

    Created the entry point app.js and added script property to the package.json to auto run this file.

---
+++ app.js
@@ -0,0 +1,17 @@
+// Require packages and set the port
+const express = require('express');
+const port = 3002;
+const app = express();
+
+// look for a GET request on the root (/) of the server
+app.get('/', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.send('Hello, Server!');
+});
+
+// Start the server
+const server = app.listen(port, (error) => {
+if (error) return console.log(`Error: ${error}`);
+
+console.log(`Server listening on port ${server.address().port}`);
+});
